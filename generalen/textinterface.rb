c0d9cba4f14103f570f3c73b65fa31fbbd4293f8
Nicklas Lindgren <nili@modio.se>
2016-06-18 00:35:56 +0200

    Add Slack bot interface

--- generalen/textinterface.rb
+++ generalen/textinterface.rb
@@ -579,7 +579,7 @@ module TextInterface
   end
 
   def help(words = nil)
-    text = "Jag leder partier av ett spel inte helt olikt Risk.\n\nFraser jag potentiellt begriper:\n"
+    text = "(Sorry, i only speak swedish.)\n\nJag leder partier av ett spel inte helt olikt Risk.\n\nFraser jag potentiellt begriper:\n"
     phrases = ([ "partier",
                  "nytt parti [SPEL]",
                  "deltag i [SPEL]",
@@ -605,7 +605,7 @@ module TextInterface
                  "uppdrag" ]).sort
     text << phrases.column_list_view
     text << "\nNamn och fraser går att kom-förkorta varhelst man vill."
-    text << "\n\nFramför klagomål till <person 9023: Nicklas Lindgren (Äter mopeder, öppnar kasino)>\n\nGurk. Ost."
+    text << "\n\nFramför klagomål till #{$settings[:admin]}"
     post text
   end
 
